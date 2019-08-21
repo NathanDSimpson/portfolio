6490d2d085368c895782bf949bc3d5ff8f4321f8
Nathan Simpson <ndsimpson01@gmail.com>
2019-08-21 11:15:07 -0600

    Implimented SCSS

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+
+class NavBar extends Component{
+    render(){
+        return(
+            <div>
+                <div>Nathan SIMPSON</div>
+                <span>
+                    <div>Home</div>
+                    <div>About</div>
+                    <div>Skills</div>
+                    <div>Projects</div>
+                    <div>Contact</div>
+                </span>
+            </div>
+        )
+    }
+}
+
+export default NavBar
