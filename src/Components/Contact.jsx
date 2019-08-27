0cab6c0822897285400055dfce6f3dfb143ef724
Nathan Simpson <ndsimpson01@gmail.com>
2019-08-27 16:52:34 -0600

    Basic contact form

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -2,10 +2,46 @@ import React, {Component} from 'react'
 import { withRouter } from 'react-router-dom'
 
 class Contact extends Component{
+    state = {
+        name: '',
+        email: '',
+        message: ''
+    }
+
+    handleInput = event => {
+        let {name, value} = event.target
+        this.setState({
+            [name]: value
+        })
+    }
+
     render(){
         return(
             <div>
-                Contact Component
+                <form>
+                    <div>
+                    <input 
+                        onChange={this.handleInput} 
+                        type="text" 
+                        name='name' 
+                        placeholder='Name'
+                    />                    
+                    <input 
+                    onChange={this.handleInput} 
+                    type="text" 
+                    name='email' 
+                    placeholder='Email'
+                />
+                    </div>
+                    <div>
+                    <input 
+                        onChange={this.handleInput} 
+                        type="text" 
+                        name='message' 
+                        placeholder='Message'
+                    />
+                    </div>
+                </form>
             </div>
         )
     }
